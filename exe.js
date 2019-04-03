17756a61ec00399d4053172817fc8685cdb897ee
Efrain <efleivatobias@s.sfusd.edu>
2019-04-03 16:22:03 -0700

    Update exe.js

--- exe.js
+++ exe.js
@@ -9,39 +9,18 @@ console.log('GO WORK!!!');
         block.move(posX,posY);
         block.stroke("black");
     }
-    for(let i=0; i<9;i++){
+    let x=80;
+    let y=575;
+
+    for(let k=9; k>=1; k--){
+      for(let i=0; i<k; i++){
         let s=i*50;
-        drawblock("brown",s+80,575);
+        drawblock("lime",s+x,y);
+      }
+      x+=25;
+      y-=25;
     }
-    for(let i=0; i<8;i++){
-        let s=i*50;
-        drawblock("red",s+105,550);
-    }
-    for(let i=0; i<7;i++){
-        let s=i*50;
-        drawblock("orange",s+130,525);
-    }
-    for(let i=0; i<6;i++){
-        let s=i*50;
-        drawblock("yellow",s+155,500);
-    }
-    for(let i=0; i<5;i++){
-        let s=i*50;
-        drawblock("lime",s+180,475);
-    }
-    for(let i=0; i<4;i++){
-        let s=i*50;
-        drawblock("green",s+205,450);
-    }
-    for(let i=0; i<3;i++){
-        let s=i*50;
-        drawblock("blue",s+230,425);
-    }
-    for(let i=0; i<2;i++){
-        let s=i*50;
-        drawblock("purple",s+255,400);
-    }
-    drawblock("gold",280,375);
+    
     var polygon = draw.polygon('0,0 40,40 100,0 60,40 100,100 60,60 0,100 40,60');
     polygon.fill('cyan').move(250,140);
     var line = draw.line(305,375, 305,200);
